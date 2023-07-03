c73cecf33ba9fd8210c52e6c80a2331c592e4b60
korakopi <132523651+leocode09@users.noreply.github.com>
2023-07-03 08:55:12 +0200

    player controlls

--- numbers_display.js
+++ numbers_display.js
@@ -1,6 +1,57 @@
 const numbers = document.querySelector('#numbers')
-for (const rand of random) {
-    numbers.innerHTML += `
-        <div class="number">${rand}</div>
+
+function zeros(number) {
+    const numberString = number.toString();
+  
+    if (numberString.length >= 2) {
+      return numberString;
+    } else {
+      const zerosToAdd = 2 - numberString.length;
+      const paddedNumber = '0'.repeat(zerosToAdd) + numberString;
+      return paddedNumber;
+    }
+}
+  
+let numbers_str = ``
+for (let i = 0; i < 100; i++) {    
+    numbers_str += /*html*/ `
+        <div class="number" data-i="${zeros(i)}">
+            ${zeros(rand[i])}
+        </div>
     `
+}
+numbers.innerHTML = numbers_str
+
+let pos = 0
+
+onkeydown = e => {
+    const arrowKeys = {
+        ArrowUp: () => pos-=10,
+        ArrowDown: () => pos+=10,
+        ArrowLeft: () => pos-=1,
+        ArrowRight: () => pos+=1,
+    };
+    if (arrowKeys[e.key]) arrowKeys[e.key]();
+    slide(pos, centers)
+    console.log(pos);
+}
+
+const number_list = [... document.querySelectorAll('.number')]
+let centers = []
+number_list.forEach(num => {
+    let properties = num.getBoundingClientRect()
+    centers.push({
+        x: properties.left+(properties.width/2),
+        y: properties.top+(properties.height/2)
+    })
+});
+console.log(number_list);
+console.log(centers);
+
+let mover = document.querySelector('.mover')
+function slide(pos, centers) {
+    mover.setAttribute('style', `
+        top: ${centers[pos].y}px;
+        left: ${centers[pos].x}px
+    `)
 }
--- random_numbers.js
+++ random_numbers.js
@@ -1,10 +1,10 @@
 let ordered = Array.from(Array(100).keys(), i=>i+1)
-let random = []
+let rand = []
 
 for (let i = 0; i < 100; i++) {
     let index = Math.floor(Math.random()*ordered.length)
-    random.push(ordered[index])
+    rand.push(ordered[index])
     ordered.splice(index,1)
 }
 
-console.log(random)
+console.log(rand)
