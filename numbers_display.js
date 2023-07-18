3666da0667eae70027f6b6a164a52d99786aea51
korakopi <132523651+leocode09@users.noreply.github.com>
2023-07-18 16:13:35 +0200

    >_  single player complete

--- numbers_display.js
+++ numbers_display.js
@@ -1,4 +1,13 @@
-const numbers = document.querySelector('#numbers')
+init_number()
+const request_ele = document.querySelector('#request')
+const number_list = [... document.querySelectorAll('.number')]
+const mover = document.querySelector('.mover')
+
+let centers = []
+let pos = 0
+let request = 1
+
+request_ele.textContent = 'Find: '+request
 
 function zeros(number) {
     const numberString = number.toString();
@@ -11,34 +20,44 @@ function zeros(number) {
       return paddedNumber;
     }
 }
-  
-let numbers_str = ``
-for (let i = 0; i < 100; i++) {    
-    numbers_str += /*html*/ `
-        <div class="number" data-i="${zeros(i)}">
-            ${zeros(rand[i])}
-        </div>
-    `
+
+function init_number() {
+    const numbers = document.querySelector('#numbers')
+    let numbers_str = ``
+    for (let i = 0; i < 100; i++) {    
+        numbers_str += /*html*/ `
+            <div class="number" data-i="${zeros(i)}">
+                ${zeros(rand[i])}
+            </div>
+        `
+    }
+    numbers.innerHTML = numbers_str
 }
-numbers.innerHTML = numbers_str
 
-let pos = 0
 
-onkeyup = e => {
+onkeydown = e => {
     const arrowKeys = {
         ArrowUp: () => (pos < 10)? pos+=90:pos-=10,
         ArrowDown: () => (pos > 89)? pos-=90:pos+=10,
         ArrowLeft: () => (zeros(pos)[1] == 0)? pos+=9:pos-=1,
-        ArrowRight: () => (zeros(pos)[1] == 9)? pos-=9:pos+=1
+        ArrowRight: () => (zeros(pos)[1] == 9)? pos-=9:pos+=1,
+        Enter: () => {
+            if (rand[pos]==request) {
+                request++
+                request_ele.textContent = 'Find: '+request
+                number_list[pos].style.color = '#d70000'
+            }
+        }
     };
     if (arrowKeys[e.key]) arrowKeys[e.key]();
     slide(pos, centers)
-    console.log(pos);
 }
 
-const number_list = [... document.querySelectorAll('.number')]
-let centers = []
-onresize = () => {
+
+onresize = number_location
+number_location()
+function number_location() {
+    console.log('mmm');
     centers = []
     number_list.forEach(num => {
         let properties = num.getBoundingClientRect()
@@ -46,50 +65,21 @@ onresize = () => {
             x: properties.left+(properties.width/2),
             y: properties.top+(properties.height/2)
         })
-    });
-    slide(pos, centers)
-    console.log(centers);
-}
-number_list.forEach(num => {
-    let properties = num.getBoundingClientRect()
-    centers.push({
-        x: properties.left+(properties.width/2),
-        y: properties.top+(properties.height/2)
     })
-});
-console.log(number_list);
-console.log(centers);
+    slide(pos, centers)   
+}
 
-let mover = document.querySelector('.mover')
-
-
-slide(pos, centers)
 function slide(pos, centers) {
-
-    mover.setAttribute('style', `
-        top: ${centers[pos].y}px;
-        left: ${centers[pos].x}px;
-        width: 20px;
-        height: 20px;
-
-    `)
-
-    setTimeout(() => {  
-        mover.setAttribute('style', `
-            top: ${centers[pos].y}px;
-            left: ${centers[pos].x}px;
-            width: 50px;
-            height: 50px;
-        `)
-    }, 100);
+    mover.style.top = centers[pos].y+'px'
+    mover.style.left = centers[pos].x+'px'
 }
   
-  function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (i=0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
-  }
+}
   
